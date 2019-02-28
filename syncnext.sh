ed8cf1cb9706a585407b9e58b4d374a018d010d9
daslkfj@sdjf;s.com <daslkfj@sdjf;s.com>
2019-02-28 21:42:00 +0200

    dafj;

--- syncnext.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export ETCDCTL_API=3
-cd /TopStor/
-theone=` echo $@ | awk '{print $1}'`;
-thetwo=` echo $@ | awk '{print $2}'`;
-myip=`/sbin/pcs resource show CC | grep Attributes | awk -F'ip=' '{print $2}' | awk '{print $1}'`
-./etcdsyncnext.py $myip $theone $thetwo 2>/dev/null
