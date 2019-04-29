5be8a7275713a386845f8a1d99601a7017ce6439
dkfjl@dljf <dkfjl@dljf>
2019-04-29 17:00:10 +0200

    dfj;

--- startzfs.sh
+++ startzfs.sh
@@ -155,6 +155,7 @@ else
   systemctl restart chronyd
   ./etcdsync.py $myip primary primary 2>/dev/null
   ./etcddellocal.py $myip known --prefix 2>/dev/null
+  ./etcddellocal.py $myip activepool --prefix 2>/dev/null
   ./etcddellocal.py $myip localrun --prefix 2>/dev/null
   ./etcddellocal.py $myip run --prefix 2>/dev/null
   ./etcddellocal.py $myip pools --prefix 2>/dev/null
@@ -162,6 +163,7 @@ else
   ./etcddellocal.py $myip poolsnxt --prefix 2>/dev/null
   ./etcddellocal.py $myip vol --prefix 2>/dev/null
   ./etcdsync.py $myip known known 2>/dev/null
+  ./etcdsync.py $myip activepool activepool 2>/dev/null
   ./etcdsync.py $myip pools pools 2>/dev/null
   ./etcdsync.py $myip poolsnxt poolsnxt 2>/dev/null
   ./etcdsync.py $myip namespace namespace 2>/dev/null
