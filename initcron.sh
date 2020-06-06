feced3d3c0e3ce4448dfa1a7300c07f97c672f77
dkafjdk.sdfj <dkjafl@sldkfj.com>
2020-06-06 13:12:15 +0200

    fixing

--- initcron.sh
+++ initcron.sh
@@ -22,4 +22,8 @@ sleep 120
 /sbin/ip addr del 10.11.11.254/24 dev $nic 
 /TopStor/factory.sh
 
+crontab -l | grep -v Initialization > /TopStordata/cronthis
+echo "0" "0" "1" "*" "*"  sh /TopStor/clearlog.sh Initialization >> /TopStordata/cronthis
+echo "0" "*/6" "*" "*" "*"  sh /TopStor/initcleandb.sh  Initialization >> /TopStordata/cronthis
+crontab /TopStordata/cronthis
 
