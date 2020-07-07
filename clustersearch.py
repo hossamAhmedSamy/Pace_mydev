6d460a78b4206a52f363586a005044fe98df4c8b
dksafj <yodusaf@sljdf>
2020-07-07 22:40:28 +0200

    fixing

---
+++ clustersearch.py
@@ -0,0 +1,27 @@
+#!/bin/python3.6
+import sys, subprocess
+from etcdgetlocal import etcdget as getlocal
+
+ip=sys.argv[1].split('.')
+serverstatus='nothing'
+x=getlocal(sys.argv[1],'clusternode','--prefix')
+#for host in x:
+# cmdline=['etcdctl','-w','json','--endpoints='+host[1]+':2379','member','list','2>/dev/null']
+#subprocess.run('export','ETCDCTL_API=3')
+for host in x:
+ cmdline=['etcdctl','-w','json','--endpoints='+host[1]+':2379','member','list']
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ if result.returncode==0:
+  etcdserver=host[1]
+  serverstatus=result.stdout
+serverstatus=str(serverstatus)[2:]
+serverstatus=serverstatus[:-3]
+if len(serverstatus) < 6:
+ serverstatus='nothing'
+etcdfile=open('/pacedata/runningetcdnodes.txt','w')
+etcdfile.write(serverstatus)
+etcdfile.close()
+etcdfile=open('/var/www/html/des20/Data/runningetcdnodes.txt','w')
+etcdfile.write(serverstatus)
+etcdfile.close()
+print(serverstatus)
