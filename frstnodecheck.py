445c64810f151a5ce65211205bf782a4029f7613
dkjlfsadl@sldkfj.com <dkjlfsadl@sldkfj.com>
2020-07-20 02:26:14 +0300

    j;

---
+++ frstnodecheck.py
@@ -0,0 +1,14 @@
+#!/bin/python3.6
+import sys, subprocess, socket
+from etcdget import etcdget as get
+
+myhost=socket.gethostname()
+leader=get('leader')
+if myhost in str(leader):
+ frstnode=get('frstnode')[0].split('/')[0]
+ if myhost not in frstnode:
+  frstnodeip=get('Active',frstnode)[0][1]
+  cmdline=['etcdctl','-w','json','--endpoints='+frstnodeip+':2379','member','list']
+  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+  if result.returncode==0:
+   /TopStor/rebootme 
