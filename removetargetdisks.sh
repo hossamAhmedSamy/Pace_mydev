ee601842f9b9a372dcd636fdce06f496b160ce0d
dksafj <yodusaf@sljdf>
2020-07-25 00:16:52 +0300

    dfaj;

--- removetargetdisks.sh
+++ removetargetdisks.sh
@@ -9,4 +9,6 @@ if [ $? -ne 0 ];
 then
  initiator=`targetcli ls | grep $thehost | grep 1994 | awk -F'o- ' '{print $2}' | awk '{print $1}'`
  targetcli iscsi/iqn.2016-03.com.$hostname:t1/tpg1/acls/ delete $initiator
+else
+ targetcli clearconfig confirm=true
 fi
