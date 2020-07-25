aa546a71516594c9d7a229b8ef34bbd1b6e7b930
dkjlfsadl@sldkfj.com <dkjlfsadl@sldkfj.com>
2020-07-25 15:37:48 +0300

    fkskjf

--- removetargetdisks.sh
+++ removetargetdisks.sh
@@ -3,12 +3,15 @@ export ETCDCTL_API=3
 cd /TopStor/
 echo $@ > /root/removetargetdisks
 thehost=`echo $@ | awk '{ print $1 }'`;
+thehostip=`echo $@ | awk '{ print $2 }'`;
 hostname=`hostname -s`
 echo $thehost | grep $hostname
 if [ $? -ne 0 ];
 then
  initiator=`targetcli ls | grep $thehost | grep 1994 | awk -F'o- ' '{print $2}' | awk '{print $1}'`
  targetcli iscsi/iqn.2016-03.com.$hostname:t1/tpg1/acls/ delete $initiator
+ iscsiadm -m node -p $thehostip:3260 -u
 else
  targetcli clearconfig confirm=true
+ iscsiadmin -m node -u
 fi
