68a145d238eeb0758204066615614fef8d02d9cf
dlksj.ewjf <dklljfd@sldjf.com>
2022-06-25 03:03:53 +0300

    fxiing

--- checksyncs.py
+++ checksyncs.py
@@ -13,7 +13,7 @@ from usersyncall import usersyncall
 from groupsyncall import groupsyncall
 from socket import gethostname as hostname
 
-syncs = ['Snapperiod','ready','leader','alias', 'user','group','evacuatehost','dataip','tz','ntp','gw','dnsname','dnssearch', 'namespace', 'known', 'allowedPartners', 'activepool', 'ipaddr', 'pools', 'poolsnxt', 'namespace', 'volumes', 'dataip', 'localrun', 'logged','uplogged', 'uplogged', 'ActivePartners', 'config', 'Parnter', 'pool', 'nextlead', 'snapperiod']
+syncs = ['Snapperioddel','Snapperiod','ready','leader','alias', 'user','group','evacuatehost','dataip','tz','ntp','gw','dnsname','dnssearch', 'namespace', 'known', 'allowedPartners', 'activepool', 'ipaddr', 'pools', 'poolsnxt', 'namespace', 'volumes', 'dataip', 'localrun', 'logged','uplogged', 'uplogged', 'ActivePartners', 'config', 'Parnter', 'pool', 'nextlead', 'snapperiod']
 collectedsyncs = ['alias']
 myhost = hostname()
 actives = get('ActivePartners','--prefix')
@@ -63,6 +63,12 @@ def checksync(myip='nothing'):
     elif sync in ['dataip','tz','ntp','gw','dnsname','dnssearch','alias']:
      cmdline='/TopStor/pump.sh HostManualconfig'+sync+'local ll'
      result=subprocess.check_output(cmdline.split(),stderr=subprocess.STDOUT).decode('utf-8')
+    elif 'Snapperioddel' in sync:
+     partner=maxgsync[0].split('perioddel')[1]
+     userreq=maxgsync[0].split('perioddel')[2]
+     cmdline='/TopStor/pump.sh PartnerDel '+partner+' yes '+userreq 
+     result=subprocess.check_output(cmdline.split(),stderr=subprocess.STDOUT).decode('utf-8')
+     
     elif sync in [ 'Snapperiod','ready','leader','known', 'allowedPartners', 'activepool', 'ipaddr', 'pools', 'poolsnxt', 'namespace', 'volumes', 'dataip', 'localrun', 'logged','uplogged', 'uplogged', 'ActivePartners', 'config', 'Parnter', 'pool', 'nextlead', 'snapperiod']:
      print('normal known leader..etc')
      cmdline='/TopStor/pump.sh etcdsync.py '+hostip+' '+sync+' '+sync
