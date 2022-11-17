e0144cabebe96abec45faeec90b09064abaeb0cc
sdlfkj <dkdjkl@dljkf.com>
2022-11-18 02:50:02 +0300

    fxlfjk

--- broadcasttolocal.py
+++ broadcasttolocal.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-import subprocess,sys, datetime, os
-import json
-from etcdget import etcdget as get
-from ast import literal_eval as mtuple
-from socket import gethostname as hostname
-from etcdputlocal import etcdput as putlocal 
+import subprocess,sys, os
+from etcdgetlocal import etcdgetlocal as get
+from etcdput import etcdput as put 
 
 def broadcasttolocal(*args):
  os.environ['ETCDCTL_API']= '3'
  knowns=[]
  knowninfo=get('known','--prefix')
  for k in knowninfo:
-  putlocal(k[1],args[0],args[1])
+  put(k[1],args[0],args[1])
 
 if __name__=='__main__':
  broadcasttolocal(*sys.argv[1:])
