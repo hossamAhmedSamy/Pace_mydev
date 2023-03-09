f0a108632fa838c03e11d01775ae6bd56542651e
sdlfkj <dkdjkl@dljkf.com>
2023-03-09 21:27:04 +0300

    fsi

---
+++ diskref.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+# needed the operands to be like : one:two:three:four
+#
+leader=`echo $@ | awk '{print $1}'`
+leaderip=`echo $@ | awk '{print $2}'`
+myhost=`echo $@ | awk '{print $3}'`
+myhostip=`echo $@ | awk '{print $4}'`
+echo $leader | grep $myhost
+if [ $? -eq 0 ];
+then
+	etcdip=$leaderip
+else
+	etcdip=$myhostip
+fi
+/pace/iscsirefresh.sh $etcdip $myhost
