c1a9f9a53ec2f620b81f01846f8aec6ad219ebda
sdlfkj <dkdjkl@dljkf.com>
2023-03-12 18:36:43 +0300

    fxing

--- checksyncs.py
+++ checksyncs.py
@@ -16,7 +16,7 @@ dirtydic = { 'pool': 0, 'volume': 0 }
 syncanitem = ['dirty','hostdown', 'diskref', 'replipart','evacuatehost','Snapperiod', 'cron','UsrChange', 'GrpChange', 'user','group','ipaddr', 'namespace', 'tz','ntp','gw','dns','cf' ]
 forReceivers = [ 'user', 'group' ]
 special1 = [ 'passwd' ]
-wholeetcd = [ 'Partnr', 'Snappreiod','leader', 'running','volumes','ready','known' ]
+wholeetcd = [ 'needtoreplace','Partnr', 'Snappreiod','leader', 'running','volumes','ready','known' ]
 etcdonly = [ 'cleanlost','balancedtype','sizevol', 'alias', 'hostipsubnet', 'allowedPartners','activepool', 'poolsnxt','pools', 'localrun','logged','ActivePartners','configured','pool','nextlead']
 syncs = etcdonly + syncanitem + special1 + wholeetcd
 ##### sync request etcdonly template: sync/Operation/ADD/Del_oper1_oper2_../request Operation_stamp###########
