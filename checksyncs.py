18e82385e5c8f16b7d29ac1c826e909919323414
sdlfkj <dkdjkl@dljkf.com>
2023-05-06 11:58:17 +0300

    fixing

--- checksyncs.py
+++ checksyncs.py
@@ -242,7 +242,7 @@ def syncrequest(leader,leaderip,myhost, myhostip):
    if myhost != leader:
     put(myhostip, syncleft+'/'+myhost, stamp)
     put(myhostip, syncleft, stamp)
- cmdline = '/TopStor/getcversion.sh '+leaderip+' '+leader+' '+myhost
+ cmdline = '/TopStor/getcversion.sh '+leaderip+' '+leader+' '+myhost+' '+'checksync'
  subprocess.check_output(cmdline.split(),stderr=subprocess.STDOUT)
  if myhost != leader:
   dones = get(leaderip,'sync','/request/dhcp')
