e4a5753680138466d56a2bc4eaa353c63558e343
sdlfkj <dkdjkl@dljkf.com>
2023-05-19 02:37:58 +0200

    fixing

--- senddiscovery.sh
+++ senddiscovery.sh
@@ -4,10 +4,10 @@ myhost=`hostname`
 myhostip=`docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip`
 while true
 do
-	./etcdput.py 10.11.11.253 possible/$myhost $myhostip 2>/dev/null
-	clusterip=`./etcdget.py 10.11.11.253 tojoin/$myhost`
-	clusteripn=`echo $clusterip | wc -c`
-	if [ $clusteripn -ge 6 ];
+	clusterip=`./etcdput.py 10.11.11.253 possible/$myhost $myhostip`
+	#clusterip=`./etcdget.py 10.11.11.253 tojoin/$myhost`
+	echo $clusterip | grep 'not reach'
+	if [ $? -ne 0 ];
 	then
 		break
 	fi
