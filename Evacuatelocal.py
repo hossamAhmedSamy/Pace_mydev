e36654890d46d49ece82dcb437c6908c741ecb95
sdlfkj <dkdjkl@dljkf.com>
2023-07-28 15:35:11 +0300

    fixing

--- Evacuatelocal.py
+++ Evacuatelocal.py
@@ -1,48 +1,20 @@
 #!/usr/bin/python3
 import subprocess,sys, datetime
-from logqueue import queuethis
-from etcdput import etcdput as put 
-from time import sleep
-from etcdputlocal import etcdput as putlocal
-from etcdgetpy import etcdget as get 
-from etcdgetlocalpy import etcdget as getlocal
 from etcddel import etcddel as deli 
-from etcddellocal import etcddel as delilocal 
-from socket import gethostname as hostname
-import logmsg
+
+
 def setall(hostn,hostip,leader):
- with open('/pacedata/perfmon','r') as f:
-  perfmon = f.readline()
- if '1' in perfmon:
-  queuethis('Evacuate','running','system')
- myhost=hostname()
- cmdline=['/pace/getmyip.sh']
- myip=subprocess.run(cmdline,stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','')
+ cmdline='docker exec etcdclient /TopStor/etcdgetlocal.py clusternode'
+ myhost=subprocess.run(cmdline.split(),stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','').replace(' ','')
+ cmdline='docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip'
+ myip=subprocess.run(cmdline.split(),stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','').replace(' ','')
  if myhost in hostn:
-  if myhost in leader:
-   cmdline=['/TopStor/Converttolocal.sh',myip]
-   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  cmdline=['/TopStor/resettarget.sh',myhost]
+  cmdline=['/TopStor/docker_setup.sh','reset']
   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  delilocal(myip,"",hostn)
-  while True:
-   cmdline=['/TopStor/rebootme','reset']
-   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-   sleep(10)
  else:
-  if myhost in leader:
-    print('iam here', hostn, hostip)
     cmdline=['/pace/removetargetdisks.sh', hostn, hostip]
     result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-    deli("",hostn)
-  else:
-    cmdline=['/pace/removetargetdisks.sh', hostn, hostip]
-    result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-    delilocal(myip,"",hostn)
- logmsg.sendlog('Evacuaesu01','info','system',hostn)
- if '1' in perfmon:
-  queuethis('Evacuate','stop','system')
- return
+    deli(myip,"",hostn)
 
 if __name__=='__main__':
  setall(*sys.argv[1:])
