ed996bc6f45f0be3d1c0a6b2955136cbdee28b0c
sdlfkj <dkdjkl@dljkf.com>
2023-07-29 06:47:02 +0300

    fixing

--- rebootmepls.sh
+++ rebootmepls.sh
@@ -3,15 +3,23 @@ cd /pace/
 export ETCDCTL_API=3
 leaderip=`echo $@ | awk '{print $1}'`
 myhost=`echo $@ | awk '{print $2}'`
+myhostip=`docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip`
+leader=`docker exec etcdclient /TopStor/etcdgetlocal.py leader`
 iswait='isreboot'`/pace/etcdget.py $leaderip rebootwait/$myhost`
 echo $isreboot | grep pls
 if [ $? -ne 0 ];
 then
 	isreboot='isreboot'`/pace/etcdget.py $leaderip rebootme/$myhost`
-	echo $isreboot | grep pls
+	echo $myhost | grep $leader
 	if [ $? -eq 0 ];
 	then
- 		configured=`/pace/etcdget.py $leaderip configured/$myhost`
+		isrebootlocal='isreboot'`/pace/etcdget.py $myhostip rebootme/$myhost`
+ 		configuredlocal=`/pace/etcdget.py $myhostip configured/$myhost`
+	fi
+	echo ${isreboot}$isrebootlocal | grep pls
+	if [ $? -eq 0 ];
+	then
+ 		configured=`/pace/etcdget.py $leaderip configured/$myhost`$configuredlocal
 		echo S$configured  | egrep 'yes|reset'
 		if [ $? -ne 0 ];
 		then
