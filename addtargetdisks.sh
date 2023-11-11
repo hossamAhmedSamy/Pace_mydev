cba233f1b0a0c007cfa0a1b52ce707353188eed8
sdlfkj <dkdjkl@dljkf.com>
2023-11-11 17:00:46 +0300

    fixing

--- addtargetdisks.sh
+++ addtargetdisks.sh
@@ -3,6 +3,15 @@
 #exit
 ##########################
 echo $@ > /root/addtargets
+bootdisk='sda'
+bootdiskf='/TopStordata/bootdiskf'
+if [ ! -f $bootdiskf ];
+then
+ echo $bootdisk > $bootdiskf
+else
+ bootdisk=`cat $bootdiskf`
+fi
+ 
 cd /pace
 etcdip=`echo $@ | awk '{print $1}'`
 myhost=`echo $@ | awk '{print $2}'`
@@ -15,16 +24,15 @@ myip=`docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip`
 mycluster=`nmcli conn show mycluster | grep ipv4.addresses | awk '{print $2}' | awk -F'/' '{print $1}'`
 declare -a iscsitargets=(`docker exec etcdclient /pace/iscsiclients.py $etcdip | grep target | awk -F'/' '{print $2}'`);
 currentdisks=`targetcli ls /iscsi`
-disks=(`lsblk -nS -o name,serial,vendor | grep -v sr0 |  grep -v LIO | awk '{print $1}'`)
+disks=(`lsblk -nS -o name,serial,vendor | grep -v $bootdisk | grep -v sr0 |  grep -v LIO | awk '{print $1}'`)
 nodes=(`docker exec etcdclient /TopStor/etcdgetlocal.py Active --prefix | awk -F'Partners/' '{print $2}' | awk -F"'" '{print $1}'`)
-diskids=`lsblk -nS -o name,serial,vendor | grep -v sr0 | grep -v LIO | awk '{print $1" "$2}'`
+diskids=`lsblk -nS -o name,serial,vendor | grep -v $bootdisk | grep -v sr0 | grep -v LIO | awk '{print $1" "$2}'`
 mappedhosts=`targetcli ls /iscsi | grep Mapped`;
 targets=`targetcli ls backstores/block | grep -v deactivated |  grep dev | awk -F'[' '{print $2}' | awk '{print $1}'`
 blocks=`targetcli ls backstores/block `
-#echo hi2 $myhost >> /root/targetadd
-echo targets $blocks
+# filter the new iscsi disks that were not part of the backstore , then create them if needed
 for ddisk in  "${disks[@]}"; do
-	echo $blocks | grep $ddisk
+	echo $blocks | grep $ddisk >/dev/null
 	if [ $? -ne 0 ];
 	then
 		echo $ddisk not a part in the targets
@@ -32,116 +40,84 @@ for ddisk in  "${disks[@]}"; do
   		targetcli backstores/block create ${ddisk}-${myhost} /dev/disk/by-id/$scsidisk
 		
 	else
-		echo $ddisk is a part in the targets
+		echo $ddisk is a part in the targets backstore
 	fi
 done
-
-#echo hi3 >> /root/targetadd
 declare -a newdisks=();
-#for node in "${nodes[@]}"; do
-# echo $mappedhosts | grep $node
-# if [ $? -ne 0 ];
-# then
-  #nodeip=`docker exec etcdclient /TopStor/etcdgetlocalpy ready/$node`
-#  targetcli iscsi/ create iqn.2016-03.com.${node}:t1 
-#  targetcli iscsi/iqn.2016-03.com.${node}:t1/tpg1/portals delete 0.0.0.0 3260
-#  targetcli iscsi/iqn.2016-03.com.${node}:t1/tpg1/portals create $myip 3266
-# fi
-#done
-targetcli ls iscsi/ | grep ".$myhost:t1" &>/dev/null
-if [ $? -ne 0 ]; then
- echo hicreate $myhost >> /root/targetadd
-
- targetcli iscsi/ create iqn.2016-03.com.${myhost}:t1 
- targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals delete 0.0.0.0 3260
- targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals create $myip 3266
- change=1
+# check and create the t1 entry of myhost
+targetcli ls iscsi/ | grep $myhost:t1 >/dev/null
+if [ $? -ne 0 ];
+then
+ targetcli iscsi/ create iqn.2016-03.com.$myhost:t1
+else
+	echo t1 entry is already create for $myhost
 fi
-echo `targetcli /iscsi ls` >> /root/targetadd
 tpgs1=(`targetcli ls /iscsi | grep iqn`) 
-#echo hi$tpgs1 >> /root/targetadd
-#echo hi4 >> /root/targetadd
+# check if my ip was changed so I have a wrong tpg1, of it my tpg was not created before. so I create the write tpg 
 targetcli ls iscsi/iqn.2016-03.com.$myhost:t1/tpg1/portals | grep $myip &>/dev/null
 if [ $? -ne 0 ]; then
+ targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals delete 0.0.0.0 3260
  targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals ls | grep 3266 | awk -F'o-' '{print $2}' | awk -F':' '{print $1}'
  oldip=`targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals ls | grep 3266 | awk -F'o-' '{print $2}' | awk -F':' '{print $1}'`
+ targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals delete $oldip 3260
  echo oldip=$oldip
  #targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/portals delete$olidp 3266
  targetcli iscsi/iqn.2016-03.com.$myhost:t1/tpg1/portals create $myip 3266
+else
+  echo my tpg1 portal is already created
 fi
 targetcli /iscsi/iqn.2016-03.com.${myhost}:t1 set global auto_add_mapped_luns=true
 i=0;
+# check if there is a new disk to create it as devdisk-myhsot in the backstore block
 for ddisk in "${disks[@]}"; do
  devdisk=$ddisk 
- echo ddisk===$ddisk
  idisk=`echo "$diskids" | grep -w $ddisk | awk '{print $2}'`
- echo compare devdisk-ddisk=$ddisk $idisk
- echo $currentdisks | grep $idisk &>/dev/null
+ echo $currentdisks | grep $devdisk-$myhost &>/dev/null
  if [ $? -ne 0 ]; then
-  echo Imhere
   pdisk=`ls /dev/disk/by-id/ | grep $idisk | grep -v part | grep scsi | head -1`
   targetcli backstores/block create ${devdisk}-${myhost} /dev/disk/by-id/$pdisk
   change=1
-  echo currentdisks $currentdisks
   tpgs=(`targetcli ls /iscsi | grep iqn | grep TPG | grep ':t1' | awk -F'iqn' '{print $2}' | awk '{print $1}'`)
   for iqn in "${tpgs[@]}"; do
-   echo iqn= $iqn devdisk=$devdisk-${myhost}
    ################################targetcli iscsi/iqn${iqn}/tpg1/luns/ create /backstores/block/${devdisk}-${myhost}  
    change=1
   done
+ else
+  echo $devdisk-$myhost is already created in the backstores/block 
  fi
 done;
 
-#echo hi5 >> /root/targetadd
+#######check if one of the hosts is new and was not mapped before
 for target in "${iscsitargets[@]}"; do
  echo $mappedhosts | grep $target &>/dev/null
  if [ $? -ne 0 ]; then
-  ############################targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1/acls/ create iqn.1994-05.com.redhat:$target
-  
-  #targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1 demo_mode_write_protect=0 
-  #targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1 generate_node_acls=1 
-  #targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1 set attribute authentication=1
-  #targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1 set auth userid=MoatazNegm 
-  #targetcli iscsi/iqn.2016-03.com.${myhost}:t1/tpg1 set auth password=YNPassword121 
-
   change=1
  fi
 done
+
 targetcli /iscsi/iqn.2016-03.com.${myhost}:t1 set global auto_add_mapped_luns=true
 tpgs1=(`targetcli ls /iscsi | grep iqn | grep TPG | grep ':t1'`)
 tpgs=(`targetcli ls /iscsi | grep iqn | grep TPG | grep ':t1' | awk -F'iqn' '{print $2}' | awk '{print $1}'`)
 
-#echo hi6.5 >> /root/targetadd
-for iqn in "${tpgs[@]}"; do
-	node=`echo $iqn | awk -F'.' '{print $4}' | awk -F':' '{print $1}'`
-#echo hi6 >> /root/targetadd
-	echo $actives | grep $node
-	if [ $? -ne 0 ];
-	then
-#echo '##############################################################'>> /root/targetadd
-#echo hi$tpgs1 >> /root/targetadd
-#echo hi$iqn >> /root/targetadd
-#echo hi$node >> /root/targetadd
-#echo hi$actives >> /root/targetadd
-#echo '##############################################################' >> /root/targetadd
-		echo hidelete >> /root/targetadd
-			
-		#targetcli /iscsi delete iqn$iqn
-	fi
-done	
-
-#echo hi7 >> /root/targetadd
+# mapping the luns to every iqn in the tpgs create the missing ones and create also the 
 for node in "${nodes[@]}"; do
  for ddisk in "${disks[@]}"; do
 	for iqn in "${tpgs[@]}"; do
+ 		targetcli iscsi/iqn${iqn}/tpg1/acls/ ls | grep $node >/dev/null
+		if [ $? -ne 0 ];
+		then
+  			targetcli iscsi/iqn${iqn}/tpg1/acls/ create iqn.1994-05.com.redhat:$node
+		 	echo An tpg1/acl is create for the node $node
+		else
+		 	echo the node $node already has an acl entry the tpgs 
+		fi
  		devdisk=`echo $ddisk | awk '{print $1}'`
- 		targetcli iscsi/iqn${iqn}/tpg1/luns/ ls | grep  ${devdisk}-${myhost}  
+ 		targetcli iscsi/iqn${iqn}/tpg1/luns/ ls | grep  ${devdisk}-${myhost}  >/dev/null
 		if [ $? -eq 0 ];
 		then
-			echo iqn$iqn has a map for $devdisk
+			echo iqn$iqn has a map for $devdisk-$myhost
 		else
 			echo iqn$iqn is not mapped to  $devdisk
-  			targetcli iscsi/iqn${iqn}/tpg1/acls/ create iqn.1994-05.com.redhat:$node
    			targetcli iscsi/iqn${iqn}/tpg1/luns/ create /backstores/block/${devdisk}-${myhost}  
 		fi
 	done
@@ -157,11 +133,4 @@ endingtarget=`targetcli ls | wc -l`
 if [[ $initialtarget != $endingtarget ]];
 then
   stamp=`date +%s%N`
-  #/TopStor/etcdput.py $mycluster sync/diskref/______/request diskref_$stamp
 fi
-#if [ $change -eq 1 ];
-#then
-# targetcli saveconfig /pacedata/targetconfig
-# sleep 1 
-#:wq
-#fi
