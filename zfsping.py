5e138f62ceb94feb513df4bb488d973ea6e05656
sdlfkj <dkdjkl@dljkf.com>
2024-08-17 04:47:02 +0300

    fixing

--- zfsping.py
+++ zfsping.py
@@ -369,7 +369,7 @@ if __name__=='__main__':
        zload = 0
       else:
        zload = getload(leaderip,myhost)
-       print('still load high',zload,'counter',counter)
+       print('still load high',zload,'counter',counter,'process number',i)
        sleep(2)
       print('load ok', zload)
  exit()
